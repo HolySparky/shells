77490fb8553eda03090f01d963cecd0e00500dd9
HolySparky <lalasjtu@gmail.com>
2013-12-17 11:15:19 -0800

    added real copy-key for Mac OS

---
+++ try.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#To copy the isa_id into a remote server 
+
+echo "You start with $# positional parameters"
+
+user="ubuntu"
+host=
+
+if [ "$1" != "" ];then 
+   user=$1
+   echo "user = $user"
+else
+    echo "format : [username][host1][host2]...[hostn]"
+fi
+
+shift
+
+for i in $@; do
+    host=$i
+    if ssh $user@$host test -d ~/.ssh; then
+        echo "remote folder exists"
+        cat ~/.ssh/id_rsa.pub | ssh $user@$host " cat >> ~/.ssh/authorized_keys"
+    else
+        echo "remote .ssh file not found, creating folder"
+        cat ~/.ssh/id_rsa.pub | ssh $user@$host "mkdir ~/.ssh; cat >> ~/.ssh/authorized_keys"
+    fi
+done
